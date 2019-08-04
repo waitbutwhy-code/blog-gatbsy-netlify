f564213d9ff81f6126b8b2bb22e5039ced9f0302
mjdude <mohammed.jalil@gmail.com>
2019-08-04 14:18:01 +0800

    removed package-lock

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -54,7 +54,7 @@ const Contact = () => (
       </Header>
       <Content>
         <h1>Contact</h1>
-        <p>Super cool intro text to get people contacting me! It uses Netlify's form feature.</p>
+        <p>Want to know more? Get in touch</p>
         <form name="contact-form" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="/success">
           <p>
             <label htmlFor="contact-name">
