61eab47c4613b299ba9c549dab7b777e53d38664
mo <you@example.com>
2020-06-28 01:48:32 +0800

    updated favicon

--- config/index.js
+++ config/index.js
@@ -2,13 +2,13 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
   siteTitle: 'Broconomist', // Navigation and Site Title
-  siteTitleAlt: 'Venture, technology & the future', // Alternative Site title for SEO
+  siteTitleAlt: 'Broconomist: Business, venture & the future', // Alternative Site title for SEO
   siteTitleManifest: 'Technology & The Future',
   siteUrl: 'https://broconomist.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteHeadline: 'Writing and publishing content', // Headline for schema.org JSONLD
   siteBanner: '/social/broconomist-banner.png', // Your image for og:image tag. You can find it in the /static folder
-  favicon: 'src/bro-logo-small.png', // Your image for favicons. You can find it in the /src folder
+  favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
   siteDescription: 'Analysis, opinion pieces on venture, technology and building the future.', // Your site description
   author: 'broconomist', // Author for schemaORGJSONLD
   siteLogo: '/social/bro-logo.png', // Image for schemaORGJSONLD
