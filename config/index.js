c5023f35989abc4fa1475c1b8490d93f76e3596c
mo <you@example.com>
2020-08-23 19:45:57 +0800

    updated images / banners and descriptions

--- config/index.js
+++ config/index.js
@@ -2,13 +2,13 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
   siteTitle: 'Broconomist', // Navigation and Site Title
-  siteTitleAlt: 'Broconomist: Business, venture & the future', // Alternative Site title for SEO
-  siteTitleManifest: 'Technology & The Future',
+  siteTitleAlt: 'Broconomist: Venture, finance & the future', // Alternative Site title for SEO
+  siteTitleManifest: 'Venture & the future',
   siteUrl: 'https://broconomist.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteHeadline: 'Writing and publishing content', // Headline for schema.org JSONLD
-  siteBanner: '/social/broconomist-banner.png', // Your image for og:image tag. You can find it in the /static folder
-  favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
+  siteBanner: '/social/broconomist-website-banner.png', // Your image for og:image tag. You can find it in the /static folder
+  favicon: 'src/favicon-96x96.png', // Your image for favicons. You can find it in the /src folder
   siteDescription: 'Analysis, opinion pieces on venture, technology and building the future.', // Your site description
   author: 'broconomist', // Author for schemaORGJSONLD
   siteLogo: '/social/bro-logo.png', // Image for schemaORGJSONLD
@@ -22,5 +22,5 @@ module.exports = {
   // Manifest and Progress color
   // See: https://developers.google.com/web/fundamentals/web-app-manifest/
   themeColor: '#3498DB',
-  backgroundColor: '#2b2e3c',
+  backgroundColor:  '#2b2e3c',
 }
