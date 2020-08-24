72256930c2b6d32cd543fe1356c8f19bfe042f82
mo <you@example.com>
2020-08-24 21:26:01 +0800

    added gtm

--- config/index.js
+++ config/index.js
@@ -3,13 +3,13 @@ module.exports = {
 
   siteTitle: 'Broconomist', // Navigation and Site Title
   siteTitleAlt: 'Broconomist: Venture, finance & the future', // Alternative Site title for SEO
-  siteTitleManifest: 'Venture & the future',
+  siteTitleManifest: 'Venture, finance & the future',
   siteUrl: 'https://broconomist.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
-  siteHeadline: 'Writing and publishing content', // Headline for schema.org JSONLD
+  siteHeadline: 'Venture, finance & the future', // Headline for schema.org JSONLD
   siteBanner: '/social/broconomist-website-banner.png', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
-  siteDescription: 'Analysis, opinion pieces on venture, technology and building the future.', // Your site description
+  siteDescription: 'Analysis, opinions on venture, finance and building the future.', // Your site description
   author: 'broconomist', // Author for schemaORGJSONLD
   siteLogo: '/social/bro-logo.png', // Image for schemaORGJSONLD
 
@@ -17,7 +17,7 @@ module.exports = {
   userTwitter: '@yo_jalil', // Twitter Username - Optional
   ogSiteName: 'Broconomist', // Facebook Site Name - Optional
   ogLanguage: 'en_US', // Facebook Language
-  googleAnalyticsID: 'UA-145053152-1',
+  googleAnalyticsID: 'GTM-TQJVT32',
 
   // Manifest and Progress color
   // See: https://developers.google.com/web/fundamentals/web-app-manifest/
